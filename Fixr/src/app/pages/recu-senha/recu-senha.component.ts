a8973446156d6698a9ec9d30e4ed46aea12c9986
G4htk <gabriel290303@edu.unipar.br>
2026-02-27 20:59:18 -0300

    html da tela de login

---
+++ Fixr/src/app/pages/recu-senha/recu-senha.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recu-senha',
+  imports: [],
+  templateUrl: './recu-senha.component.html',
+  styleUrl: './recu-senha.component.css'
+})
+export class RecuSenhaComponent {
+
+}
