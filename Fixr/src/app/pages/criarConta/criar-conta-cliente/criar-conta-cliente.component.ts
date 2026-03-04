3fe0e2ee07cf43349905e5ae44bccaea850e3072
G4htk <gabriel290303@edu.unipar.br>
2026-03-03 21:44:40 -0300

    criar contas cirado

---
+++ Fixr/src/app/pages/criarConta/criar-conta-cliente/criar-conta-cliente.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-criar-conta-cliente',
+  imports: [],
+  templateUrl: './criar-conta-cliente.component.html',
+  styleUrl: './criar-conta-cliente.component.css'
+})
+export class CriarContaClienteComponent {
+
+}
