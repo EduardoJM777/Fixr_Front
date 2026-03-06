6b1742f059ef73243ad1ca9be932fa897e5b0e00
Eduardo Johanns Michalski <eduardo160403@edu.unipar.br>
2026-03-06 20:19:34 -0300

    Co-authored-by: Gabriel_Amorim <G4htk@users.noreply.github.com>

--- Fixr/src/app/app.ts
+++ Fixr/src/app/app.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { CadastroComponent } from './pages/cadastro/cadastro.component';
 import { HeaderRec } from './components/header-rec/header-rec';
 import { RecuSenhaComponent } from './pages/recu-senha/recu-senha.component';
+import { CriarContaClienteComponent } from './pages/criarConta/criar-conta-cliente/criar-conta-cliente.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RecuSenhaComponent],
+  imports: [RouterOutlet, CriarContaClienteComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ Fixr/src/app/pages/criarConta/criar-conta-prestador/criar-conta-prestador.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-criar-conta-prestador',
+  imports: [],
+  templateUrl: './criar-conta-prestador.html',
+  styleUrl: './criar-conta-prestador.css',
+})
+export class CriarContaPrestador {
+
+}
