a3aaec67ee6decf92e1a0061f7a3963d39ef1d3e
Eduardo Johanns Michalski <eduardo160403@edu.unipar.br>
2026-04-09 16:41:35 -0300

    feitos testes apos merge de branch antiga

--- Fixr/src/app/app.ts
+++ Fixr/src/app/app.ts
@@ -1,13 +1,5 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { CadastroComponent } from './pages/cadastro/cadastro.component';
-import { HeaderRec } from './components/header-rec/header-rec';
-import { RecuSenhaComponent } from './pages/recu-senha/recu-senha.component';
-import { CriarContaClienteComponent } from './pages/criarConta/criar-conta-cliente/criar-conta-cliente.component';
-import { ChatVazioComponent } from "./pages/chat-vazio/chat-vazio.component";
-import { CriarContaPrestador } from './pages/criarConta/criar-conta-prestador/criar-conta-prestador';
-import { CriarProfissaoComponent } from "./pages/criarConta/criar-conta-prestador/criar-profissao/criar-profissao.component";
-import { RecuSenhaOk } from "./pages/recu-senha/recu-senha-ok/recu-senha-ok";
 
 @Component({
   selector: 'app-root',
