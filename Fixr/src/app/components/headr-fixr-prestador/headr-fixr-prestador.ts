05be29ef8b7150c17bd89c7076036f45175e01f0
G4htk <gabriel290303@edu.unipar.br>
2026-04-27 20:35:35 -0300

    feito rotas do header

--- Fixr/src/app/components/headr-fixr-prestador/headr-fixr-prestador.ts
+++ Fixr/src/app/components/headr-fixr-prestador/headr-fixr-prestador.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-headr-fixr-prestador',
+  standalone: true,
   imports: [],
   templateUrl: './headr-fixr-prestador.html',
   styleUrl: './headr-fixr-prestador.css',
 })
 export class HeadrFixrPrestador {
+  constructor(private router: Router){}
+
+  irCadastro(){
+    this.router.navigate(['/'])
+  }
+
+  irChatVazio(){
+    this.router.navigate(['/chatVazioPrestador'])
+  }
 
 }
